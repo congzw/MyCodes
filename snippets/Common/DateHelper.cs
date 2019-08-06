572029e96e3a55728b7ea6d186394715477f41f1
congzw <2981810300@qq.com>
2019-08-06 10:56:05 +0800

    add common

---
+++ snippets/Common/DateHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommonTrace.Common
+{
+    public class DateHelper
+    {
+        public string GetNowAsFormat(string format = "yyyyMMdd-HH:mm:ss")
+        {
+            var now = GetDateNow();
+            return now.ToString(format);
+        }
+
+        public Func<DateTime> GetDateDefault = () => new DateTime(2000, 1, 1);
+        public Func<DateTime> GetDateNow = () => DateTime.Now;
+        public static DateHelper Instance = new DateHelper();
+    }
+}
