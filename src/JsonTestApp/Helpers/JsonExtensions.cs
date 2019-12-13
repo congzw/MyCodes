746035e3151c4880e317f308c0795221469c6d78
congzw <2981810300@qq.com>
2019-12-13 09:15:57 +0800

    add json bags test

---
+++ src/JsonTestApp/Helpers/JsonExtensions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonTestApp.Helpers
+{
+    public interface IShouldHaveBags
+    {
+        //IDictionary<string, object> Bags { get; set; }
+    }
+
+    public interface IHaveBags : IShouldHaveBags
+    {
+        IDictionary<string, object> Bags { get; set; }
+    }
+    public interface IClientMethod : IHaveBags
+    {
+        string Method { get; set; }
+    }
+
+    public static class ClientMethodExtensions
+    {
+        public static TArgs GetArgs<TArgs>(this IClientMethod clientMethod, TArgs defaultArgs = default(TArgs))
+        {
+            if (!clientMethod.Bags.ContainsKey("args"))
+            {
+                return defaultArgs;
+            }
+
+            var bagArgs = clientMethod.Bags["args"];
+
+            if (bagArgs is JObject theJObject)
+            {
+                return theJObject.ToObject<TArgs>();
+            }
+
+            if (bagArgs is TArgs)
+            {
+                return (TArgs)bagArgs;
+            }
+
+            var json = JsonConvert.SerializeObject(bagArgs);
+            var argsT = JsonConvert.DeserializeObject<TArgs>(json);
+            return argsT;
+        }
+
+        public static void SetArgs(this IClientMethod clientMethod, object args)
+        {
+            clientMethod.Bags["args"] = args;
+        }
+
+        #region new version to be tested!
+
+        public static T GetArgsPropValue<T>(this IClientMethod clientMethodInvoke, string argsPropKey, T defaultValue)
+        {
+            var containsArgs = clientMethodInvoke.Bags.ContainsKey("args");
+            if (!containsArgs)
+            {
+                return defaultValue;
+            }
+
+            var theArgs = clientMethodInvoke.Bags["args"];
+            if (theArgs is JObject theJObject)
+            {
+                //来自网络序列化
+                var jToken = theJObject.GetValue(argsPropKey, StringComparison.OrdinalIgnoreCase);
+                return jToken == null ? defaultValue : jToken.ToObject<T>();
+            }
+
+            var argsJson = JsonConvert.SerializeObject(theArgs);
+            var argsDic = JsonConvert.DeserializeObject<IDictionary<string, object>>(argsJson);
+            foreach (var argsDicKey in argsDic.Keys)
+            {
+                if (argsDicKey.Equals(argsPropKey, StringComparison.OrdinalIgnoreCase))
+                {
+                    var propValue = argsDic[argsPropKey];
+                    var propValueJson = JsonConvert.SerializeObject(propValue);
+                    return JsonConvert.DeserializeObject<T>(propValueJson);
+                }
+            }
+            return defaultValue;
+        }
+
+        public static void SetArgsPropValue<T>(this IClientMethod clientMethodInvoke, string argsPropKey, object argsPropValue)
+        {
+            var containsArgs = clientMethodInvoke.Bags.ContainsKey("args");
+            if (!containsArgs)
+            {
+                clientMethodInvoke.Bags["args"] = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            var theArgs = clientMethodInvoke.Bags["args"];
+            if (theArgs is JObject theJObject)
+            {
+                //来自网络序列化
+                theJObject.SetPropertyContent(argsPropKey, argsPropValue);
+                return;
+            }
+
+            var argsJson = JsonConvert.SerializeObject(theArgs);
+            var argsDic = JsonConvert.DeserializeObject<IDictionary<string, object>>(argsJson);
+            argsDic[argsPropKey] = argsPropValue;
+        }
+
+        #endregion
+    }
+
+    public static class JsonExtensions
+    {
+        public static JObject SetPropertyContent(this JObject source, string name, object content)
+        {
+            var prop = source.Property(name);
+
+            if (prop == null)
+            {
+                prop = new JProperty(name, content);
+
+                source.Add(prop);
+            }
+            else
+            {
+                prop.Value = JContainer.FromObject(content);
+            }
+
+            return source;
+        }
+
+        public static IDictionary<string, object> ToDictionary(this JObject jObject)
+        {
+            var result = jObject.ToObject<Dictionary<string, object>>();
+
+            var JObjectKeys = (from r in result
+                               let key = r.Key
+                               let value = r.Value
+                               where value.GetType() == typeof(JObject)
+                               select key).ToList();
+
+            var JArrayKeys = (from r in result
+                              let key = r.Key
+                              let value = r.Value
+                              where value.GetType() == typeof(JArray)
+                              select key).ToList();
+
+            JArrayKeys.ForEach(key => result[key] = ((JArray)result[key]).Values().Select(x => ((JValue)x).Value).ToArray());
+            JObjectKeys.ForEach(key => result[key] = ToDictionary(result[key] as JObject));
+
+            return result;
+        }
+    }
+
+
+}
---
+++ src/JsonTestApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Dynamic;
+using JsonTestApp.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestJObject();
+            Console.WriteLine("---------------");
+            Console.Read();
+        }
+
+        static void TestJObject()
+        {
+            //var json = JObject.Parse(@"{name:'', foo:'Foo', isOk: true}");
+            //var mock = json.ToObject<Mock>();
+
+            //var serializeObject = JsonConvert.SerializeObject(mock);
+            //Console.WriteLine(serializeObject);
+            
+            //var blah = new Blah();
+            //blah.Bags["args"] = new Mock();
+            //var blahJson = JsonConvert.SerializeObject(blah);
+            //var blahJObject = JObject.Parse(blahJson);
+            //var blahModel = blahJObject.ToObject<Blah>();
+
+            //var blah = new Blah();
+            //blah.SetArgs(new Mock());
+            //var test = blah.GetArgs((Mock)null);
+            
+            //var blah = new Blah();
+            //blah.SetArgs(new Mock());
+            //var blahJson = JsonConvert.SerializeObject(blah);
+            //var blahJObject = JsonConvert.DeserializeObject<Blah>(blahJson);
+
+            //var test = blahJObject.GetArgs((Mock)null);
+            
+            var blah = new Blah();
+            dynamic dObj = new ExpandoObject();
+            dObj.A = "A";
+            dObj.b = "B";
+            blah.Bags["args"] = dObj;
+            var blahJson = JsonConvert.SerializeObject(blah);
+            Console.WriteLine(blahJson);
+
+            var blah2 = new Blah();
+            var dic = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            dic["A"] = "A";
+            dic["b"] = "B";
+            blah2.Bags["args"] = dic;
+
+            var mock = new Mock();
+            var mockJson = JsonConvert.SerializeObject(mock);
+            var mockDic = JsonConvert.DeserializeObject<IDictionary<string, object>>(mockJson);
+            foreach (var o in mockDic)
+            {
+                Console.WriteLine(o.Key);
+            }
+            //bad!
+            //dynamic test = dic;
+            //test.C = "C";
+            var blahJson2 = JsonConvert.SerializeObject(blah2);
+            Console.WriteLine(blahJson2);
+        }
+    }
+    
+    public class Mock
+    {
+        public string Name { get; set; }
+        public string Foo { get; set; }
+        public bool IsOk { get; set; }
+    }
+
+    public class Blah : IClientMethod
+    {
+        public Blah()
+        {
+            Bags = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public IDictionary<string, object> Bags { get; set; }
+        public string Method { get; set; }
+    }
+}
